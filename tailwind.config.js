4823d61f8d4028b3d2052938d9587f1d2bfdc018
asmashaik040 <asmashaik040@gmail.com>
2024-07-04 12:31:10 +0530

    configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js",
+  ],
+  theme: {
+    screens: {
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
